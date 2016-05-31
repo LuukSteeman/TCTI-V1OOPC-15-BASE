8f1529b52e04137fc3470581a7a5df7a951874b7
LuukSteeman <luuksteeman@live.nl>
2016-05-31 12:23:29 +0200

    17-2-huiswerk-opdracht af

---
+++ 17-2-huiswerk-opdracht-stuiteren/muur.hpp
@@ -0,0 +1,22 @@
+#ifndef MUUR_HPP
+#define MUUR_HPP
+
+#include "window.hpp"
+#include "rectangle.hpp"
+#include "vector.hpp"
+
+class muur : public rectangle {
+private:
+	bool filled;
+	int update_interval;
+	int update_count;
+	vector start_help;
+	vector bounce;
+	
+public:
+	muur (window & w, const vector & start, const vector & end, const vector & bounce, bool filled = 1, int update_interval = 100, int update_count = 0);
+	void draw() override;
+	void update() override;
+};
+
+#endif // MUUR_HPP
